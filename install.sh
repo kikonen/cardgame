ca60881b00b29ddab7fcd172624e13177f7d1879
kari <mr.kari.ikonen@gmail.com>
2013-04-24 14:49:49 +0300

    BUILD: simple setup scrupt to fetch initial dependencies

---
+++ install.sh
@@ -0,0 +1,19 @@
+echo "Fetching dependencies"
+
+#
+# For server
+#
+npm install requirejs
+npm install express
+npm install jade
+npm install backbone
+npm install underscore
+
+#
+# For building
+#
+npm install grunt
+
+#For compass:
+#https://github.com/backbone-boilerplate/grunt-bbb/issues/60
+npm install grunt-contrib-compass
