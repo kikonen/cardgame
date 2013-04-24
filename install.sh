08291d7d4508acd84aa198f443eb0c46e2269097
kari <mr.kari.ikonen@gmail.com>
2013-04-24 19:54:47 +0300

    BUILD: dependencies managed via package.json 	* npm install -l

--- install.sh
+++ install.sh
@@ -1,30 +1,7 @@
 echo "Fetching dependencies"
 
-#
-# For server
-#
-npm install requirejs
-npm install express
-npm install jade
-npm install backbone
-npm install underscore
+npm install -l
 
-#
-# For building
-#
-npm install grunt
-
-npm install grunt-contrib-cssmin
-
-#For compass:
-#https://github.com/backbone-boilerplate/grunt-bbb/issues/60
-npm install grunt-contrib-compass
-
-echo "RUN"
+echo "RUN: update gems"
 echo sudo gem update
 echo sudo gem install compass
-
-#
-# build
-#
-grunt compass:dev
